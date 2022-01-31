122a2fdd807ff8fd7bfa94d6bf01d71f426a1662
Jasper <schjaspe@amazon.de>
2022-01-31 10:34:49 +0100

    Add missing headers.

--- smily/smily/util/aws_config.py
+++ smily/smily/util/aws_config.py
@@ -1,3 +1,16 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 import configparser
 import re
 import os
@@ -7,9 +20,7 @@ from toolz import valmap
 
 ACCOUNT_ID_RE = re.compile(r"\b(\d{12})\b")
 
-config_file = Path(
-    os.environ.get("AWS_CONFIG_FILE", "~/.aws/config")
-).expanduser()
+config_file = Path(os.environ.get("AWS_CONFIG_FILE", "~/.aws/config")).expanduser()
 
 
 def _get_profiles(config):
